2fd973ecbb62294035d1dacfbcfa19ba5d2f676c
ProstoARS <arsudakov@gmail.com>
2023-09-29 15:29:03 +0300

    Check Attack

--- src/main/java/service/GameLogic.java
+++ src/main/java/service/GameLogic.java
@@ -1,15 +1,31 @@
 package service;
 
+
 import java.util.Random;
 
 public class GameLogic {
 
-
-    public static int attackModifier(int attack, int protection) {
-        return attack - protection + 1;
-    }
-
     public static int gameCubeAction() {
         return new Random().nextInt(6) + 1;
     }
+
+    public int attackModifier(int attack, int protection) {
+        return attack - protection + 1;
+    }
+
+    public boolean checkAttack(int attackModifier) {
+        boolean check = false;
+        int countCube = 0;
+        do {
+            countCube++;
+            int rsl = gameCubeAction();
+            if (rsl == 5 || rsl == 6) {
+                check = true;
+                break;
+            }
+        } while (countCube < attackModifier);
+        return check;
+    }
+
 }
+
--- src/test/java/GameLogicTest.java
+++ src/test/java/GameLogicTest.java
@@ -1,17 +1,20 @@
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import service.GameLogic;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class GameLogicTest {
 
     @Test
     public void whenAttackModifierCalculation() {
+        GameLogic gameLogic = new GameLogic();
         int attack = 5;
         int protection = 5;
-        int result = GameLogic.attackModifier(attack, protection);
+        int result = gameLogic.attackModifier(attack, protection);
         assertThat(result).isEqualTo(1);
     }
 
@@ -19,5 +22,25 @@ public class GameLogicTest {
     public void whenGameCubeAction() {
         int result = GameLogic.gameCubeAction();
         assertThat(result).isIn(List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    @Test
+    public void whenSuccessfulAttack() {
+        try (MockedStatic<GameLogic> mockedStatic = mockStatic(GameLogic.class)) {
+            mockedStatic.when(GameLogic::gameCubeAction).thenReturn(5);
+            GameLogic gameLogic = new GameLogic();
+            boolean result = gameLogic.checkAttack(3);
+            assertThat(result).isTrue();
+        }
+    }
+
+    @Test
+    public void whenFailAttack() {
+        try (MockedStatic<GameLogic> mockedStatic = mockStatic(GameLogic.class)) {
+            mockedStatic.when(GameLogic::gameCubeAction).thenReturn(3);
+            GameLogic gameLogic = new GameLogic();
+            boolean result = gameLogic.checkAttack(3);
+            assertThat(result).isFalse();
+        }
     }
 }
